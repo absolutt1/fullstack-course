9af1e12e29c8cfd69c458f1a15ea6ac160310762
Arnold Dau <arnardauletul@gmail.com>
2023-11-01 21:01:16 +0600

    videoMixer site with mouseEnter|Leave

---
+++ HWs/vmixer-script.js
@@ -0,0 +1,36 @@
+function MouseEnter(x) {
+    x.style.textDecoration = 'underline';
+    x.style.textDecorationColor = 'yellow';
+    x.style.color = 'yellow';
+}
+function MouseLeave(x) {
+    x.style.textDecoration = 'none';
+    x.style.textDecorationColor = 'none';
+    x.style.color = 'white';
+}
+function MouseEnterHomeButton(x) {
+    x.style.backgroundColor = 'white';
+}
+function MouseLeaveHomeButton(x) {
+    x.style.backgroundColor = 'yellow';
+}
+function MouseEnterMoreButton(x) {
+    x.style.color = 'white';
+    x.style.backgroundColor = 'black';
+}
+function MouseLeaveMoreButton(x) {
+    x.style.color = 'black';
+    x.style.backgroundColor = 'white';
+}
+function MouseEnterCase(x) {
+    x.style.backgroundColor = 'black';
+}
+function MouseLeaveCase(x) {
+    x.style.backgroundColor = '#7115fb';
+}
+function MouseEnterAbout(x) {
+    x.style.color = 'grey';
+}
+function MouseLeaveAbout(x) {
+    x.style.color = 'white';
+}

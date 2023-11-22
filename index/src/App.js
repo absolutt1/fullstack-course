ab618a4e4949f3675ad76b0d65cf407b67ec9578
Arnold Dau <arnardauletul@gmail.com>
2023-11-23 00:00:36 +0600

    any-react app

--- index/src/App.js
+++ index/src/App.js
@@ -1,21 +1,13 @@
-import React , {useState} from 'react';
+import React , {useEffect, useState} from 'react';
+import './App.css';
 
 const App = ( ) => {
-	const [state, setState] = useState(
-		{id: 1, name: 'Simple', },
-		{id: 2, name: 'Magnificient', },
-		{id: 3, name: 'Impressive', },
-		{id: 4, name: 'Good', },
-		{id: 5, name: 'Bad', },
-	);
 	
+
 	return (
 		<div>
-			<div></div>
-			<div className="divka"> 
-				<button>here</button> 			
-			</div>
-		</div>
+			
+		</div>	
 	)
 }
 

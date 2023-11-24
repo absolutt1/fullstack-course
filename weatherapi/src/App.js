131a1456fafa3b2cbac79235b180b5c97a5851cb
Arnold Dau <arnardauletul@gmail.com>
2023-11-24 20:43:06 +0600

    full weather api app

--- weatherapi/src/App.js
+++ weatherapi/src/App.js
@@ -31,8 +31,45 @@ const App = ( ) => {
 	return (
 		<div>
             <div>
-                <input onKeyDown={updateTxt}></input>
-                <p>{data.current?.temp_c}°C</p>
+                <input onKeyDown={updateTxt} defaultValue={"Type in your City..."}></input>
+                <div className="container">
+                    <div className="main"> 
+                        <div className="content">
+                            <p1 className="condition" style={{fontWeight: "bold"}}>{location}, {data.location?.country}</p1>
+                            <br>
+                            </br>
+                            <p1 className="condition" style={{textTransform: 'lowercase'}}>{data.current?.condition?.text}</p1>    
+                        </div>
+                        <img src={data.current?.condition?.icon}></img>
+                    </div>
+                    <div className="container-details">
+                        <div className="temp">
+                            <p1 style={{fontSize: "50px"}}>
+                                {data.current?.temp_c}°C
+                            </p1>
+                        </div>
+                        <div className="details">
+                            <p3 style={{fontWeight: "bold"}}>Details</p3>
+                            <div className="line"></div>
+                            <div className="data">
+                                <div>Feels like</div>
+                                <div style={{marginRight: '15px'}}>{data.current?.feelslike_c}°C</div>
+                            </div>
+                            <div className="data">
+                                <div>Wind</div>
+                                <div style={{marginRight: '15px'}}>{data.current?.wind_mph} m/s</div>
+                            </div>
+                            <div className="data">
+                                <div>Humidity</div>
+                                <div style={{marginRight: '15px'}}>{data.current?.humidity}%</div>
+                            </div>
+                            <div className="data">
+                                <div>Pressure</div>
+                                <div style={{marginRight: '15px'}}>{data.current?.pressure_mb} hPa</div>
+                            </div>
+                        </div> 
+                    </div>
+                </div>      
             </div>
 		</div>
 	)
